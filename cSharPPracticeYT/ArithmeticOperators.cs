ebbe61420d5071c034c9f3e1fdb2817f6886a7a8
walidulhasan <walidulhasanboniamin@gmail.com>
2023-09-28 08:31:05 +0600

    first commit

---
+++ cSharPPracticeYT/ArithmeticOperators.cs
@@ -0,0 +1,39 @@
+﻿namespace cSharPPracticeYT;
+
+public class ArithmeticOperators
+{
+    public static void Main()
+    {
+        //(+)
+        Console.WriteLine($"+ {3+2}");
+        //(-)
+        Console.WriteLine($"- {3 - 2}");
+        //(*)
+        Console.WriteLine($"* {3 * 2}");
+        //(/)
+        Console.WriteLine($"/ {3 / 2}");
+        //(%)
+        Console.WriteLine($"% {3 % 2}");
+
+
+        int number = 10;
+        int numbers = 10;
+
+        //++n Pre.
+        Console.WriteLine($"++ {++number}");//11 // 1+10;
+        //--n Pre.
+        Console.WriteLine($"-- {--number}");//10 // 1-10;
+
+        //n++ Post..
+        Console.WriteLine($"n++ {numbers++}");//10 //11 //10+1;
+        Console.WriteLine(numbers);//11
+        //n-- Post..
+        Console.WriteLine($"n-- {numbers--}");//11 //10 //10-1;
+        Console.WriteLine(numbers);
+
+
+
+        Console.ReadLine();
+
+    }
+}
